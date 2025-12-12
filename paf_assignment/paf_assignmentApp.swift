e2070c3906ce50805c63034198665e492fd9a852
Vaibhav Kukreti <9720440160beena@gmail.com>
2025-12-12 20:41:37 +0530

    Initial Commit

---
+++ paf_assignment/paf_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  paf_assignmentApp.swift
+//  paf_assignment
+//
+//  Created by Vaibhav Kukreti on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct paf_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ paf_assignmentTests/paf_assignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  paf_assignmentTests.swift
+//  paf_assignmentTests
+//
+//  Created by Vaibhav Kukreti on 12/12/25.
+//
+
+import Testing
+@testable import paf_assignment
+
+struct paf_assignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
